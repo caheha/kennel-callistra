ef94e50a6eb0f2469f9b3ddf22273635af485ea7
caheha <Actulyptius@hotmail.com>
2021-05-17 18:58:39 +0200

    cleanup + frontpage changes

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@ var navBar = document.getElementById('nav-bar')
 
 navBtn.onclick = () => {
     navBar.classList.toggle('nav-active');
-    navBtn.classList.toggle('nav-active');
+    navBtn.classList.toggle('btn-active');
 }
