2881fb118f711c11f9a9db6f46acc1078c27832e
caheha <Actulyptius@hotmail.com>
2021-05-18 20:17:42 +0200

    more subpages

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 var navBtn = document.getElementById('nav-button');
-var navBar = document.getElementById('nav-bar')
+var navBar = document.getElementById('nav-bar');
 
 navBtn.onclick = () => {
     navBar.classList.toggle('nav-active');
     navBtn.classList.toggle('btn-active');
 }
+
+
+
+var stickers = document.querySelectorAll('.sticker');
+stickers.forEach(sticker => {
+    let x = (Math.random() * 360).toFixed(2);
+    sticker.style.transform = "translate(-50%, -50%) rotate(" + x + "deg)";
+});
