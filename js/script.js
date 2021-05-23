afbf64de45b22ab28208247546326a84ecab5279
caheha <Actulyptius@hotmail.com>
2021-05-23 12:24:10 +0200

    code cleanup

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+/* ---------- Burger menu ---------- */
 var navBtn = document.getElementById('nav-button');
 var navBar = document.getElementById('nav-bar');
 
+/* Toggle active class when clicking burger menu button */
 navBtn.onclick = () => {
     navBar.classList.toggle('nav-active');
     navBtn.classList.toggle('btn-active');
@@ -8,8 +10,40 @@ navBtn.onclick = () => {
 
 
 
+
+
+/* ---------- Rotating stickers ---------- */
 var stickers = document.querySelectorAll('.sticker');
+
+/* Rotate every sticker between 0.00 and 360.00 degrees */
 stickers.forEach(sticker => {
     let x = (Math.random() * 360).toFixed(2);
     sticker.style.transform = "translate(-50%, -50%) rotate(" + x + "deg)";
 });
+
+
+
+
+
+/* ---------- Logo resizing ---------- */
+var logo = document.querySelector('.logo');
+
+/* Change logo height if scrolled > 64px & using screenwidth > 768px */
+window.onscroll = () => {
+    if (screen.width >= 768) {
+        if (window.scrollY > 64) {
+            logo.style.width = "4em";
+        } else {
+            logo.style.width = "8em";
+        }
+    }
+}
+
+/* Resize logo when resizing window to avoid large logo on mobile */
+window.onresize = () => {
+    if (screen.width >= 768) {
+        logo.style.width = "4em";
+    } else {
+        logo.style.width = "3em";
+    }
+}
--- js/slider.js
+++ js/slider.js
@@ -1,41 +1,40 @@
+/* Nova image slider */
 var containerNova = document.getElementById('img-container-nova');
 var imageCountNova = document.querySelectorAll('#img-container-nova img').length;
 var nextNova = document.getElementById('next-nova');
 var prevNova = document.getElementById ('prev-nova');
 var indexNova = 0;
 
+/* Check if next/prev image is within scope, if so, move index up or down */
 nextNova.onclick = () => { if (indexNova < imageCountNova - 3) sliderShiftNova(1) }
 prevNova.onclick = () => { if (indexNova > 0) sliderShiftNova(-1) }
 
+/* Count index up or down > calculate amount to shift >shift image-container */
 function sliderShiftNova(i){
     indexNova = indexNova + i;
     shift = containerNova.clientWidth * 1.01 / 3 * indexNova;
     containerNova.style.transform = 'translatex('+ -shift + 'px)';
 }
 
+
+
+
+
+/* Mazie image slider */
 var containerMazie = document.getElementById('img-container-mazie');
 var imageCountMazie = document.querySelectorAll('#img-container-mazie img').length;
-
 var nextMazie = document.getElementById('next-mazie');
 var prevMazie = document.getElementById ('prev-mazie');
-
 var indexMazie = 0;
 
-nextMazie.onclick = () => {
-    if (indexMazie < imageCountMazie - 3)
-        sliderShiftMazie(1)
-}
+/* Check if next/prev image is within scope, if so, move index up or down */
+nextMazie.onclick = () => { if (indexMazie < imageCountMazie - 3) sliderShiftMazie(1) }
+prevMazie.onclick = () => { if (indexMazie > 0) sliderShiftMazie(-1) }
 
-prevMazie.onclick = () => {
-    if (indexMazie > 0)
-        sliderShiftMazie(-1)
-}
-
+/* Count index up or down > calculate amount to shift >shift image-container */
 function sliderShiftMazie(i){
     indexMazie = indexMazie + i;
     shift = containerMazie.clientWidth * 1.01 / 3 * indexMazie;
-    /*(container.clientWidth / 3) * index;*/
-    /*(container.clientWidth / (100 - (imagecount - 1)))*/
     containerMazie.style.transform = 'translatex('+ -shift + 'px)';
 }
 
@@ -43,32 +42,20 @@ function sliderShiftMazie(i){
 
 
 
-
-
+/* Zelda image slider */
 var containerZelda = document.getElementById('img-container-zelda');
 var imageCountZelda = document.querySelectorAll('#img-container-zelda img').length;
-
-
 var nextZelda = document.getElementById('next-zelda');
 var prevZelda = document.getElementById ('prev-zelda');
-
-
 var indexZelda = 0;
 
-nextZelda.onclick = () => {
-    if (indexZelda < imageCountZelda - 3)
-        sliderShiftZelda(1)
-}
+/* Check if next/prev image is within scope, if so, move index up or down */
+nextZelda.onclick = () => { if (indexZelda < imageCountZelda - 3) sliderShiftZelda(1) }
+prevZelda.onclick = () => { if (indexZelda > 0) sliderShiftZelda(-1) }
 
-prevZelda.onclick = () => {
-    if (indexZelda > 0)
-        sliderShiftZelda(-1)
-}
-
+/* Count index up or down > calculate amount to shift >shift image-container */
 function sliderShiftZelda(i){
     indexZelda = indexZelda + i;
     shift = containerZelda.clientWidth * 1.01 / 3 * indexZelda;
-    /*(container.clientWidth / 3) * index;*/
-    /*(container.clientWidth / (100 - (imagecount - 1)))*/
     containerZelda.style.transform = 'translatex('+ -shift + 'px)';
 }
